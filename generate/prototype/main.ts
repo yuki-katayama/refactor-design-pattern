8cc642f8b24fd011d2d7381f6acb8162062fe909
yu-katayama <katayu810@gmail.com>
2023-08-16 07:58:03 +0900

    fix prototype

--- generate/prototype/main.ts
+++ generate/prototype/main.ts
@@ -1,78 +1,54 @@
-// プロトタイプの基底クラス。
-abstract class Shape {
-    X: number;
-    Y: number;
-    color: string;
+// コンストラクタが呼び出されないという点にも注意
+// 生成するインスタンスの種類が多すぎて、Factory（ConcreteCreator）を大量に作りたくないケース。
+// インスタンスの生成が複雑な過程を経るもので、クラスからの生成が難しい場合。
 
-    // 通常のコンストラクター。
-    constructor(source?: Shape) {
-        if (source) {
-            this.X = source.X;
-            this.Y = source.Y;
-            this.color = source.color;
-        }
-    }
+// やること
+// クラスからインスタンスをつくるのではなく、インスタンスをコピーすることで、インスタンスから別のインスタンスをつくる
+import * as clone from 'clone';
 
-    // クローン操作は、Shape のサブクラスのいずれかを返す。
-    abstract clone(): Shape;
-}
-
-// 具象プロトタイプ。
-class Rectangle extends Shape {
-    width: number;
-    height: number;
-
-    constructor(source: Rectangle) {
-        super(source);
-        this.width = source.width;
-        this.height = source.height;
-    }
-
-    clone(): Shape {
-        return new Rectangle(this);
+abstract class Animal {
+    abstract shout(): void;
+    createClone() {
+        return clone(this);
     }
 }
 
-class Circle extends Shape {
-    radius: number;
+export class Dog extends Animal {
 
-    constructor(source: Circle) {
-        super(source);
-        this.radius = source.radius;
-    }
-
-    clone(): Shape {
-        return new Circle(this);
+    public shout() {
+        console.log(`[Dog]: bowwow!`)
     }
 }
 
-// クライアント・コードのどこか。
-class Application {
-    shapes: Shape[] = [];
+export class Cat extends Animal {
 
-    constructor() {
-        const circle = new Circle({ X: 10, Y: 10, color: "", radius: 20 } as Circle);
-        this.shapes.push(circle);
-
-        const anotherCircle = circle.clone();
-        this.shapes.push(anotherCircle);
-
-        const rectangle = new Rectangle({ X: 0, Y: 0, color: "", width: 10, height: 20 } as Rectangle);
-        this.shapes.push(rectangle);
-    }
-
-    businessLogic() {
-        const shapesCopy: Shape[] = [];
-
-        for (let s of this.shapes) {
-			const clone = s.clone();
-            shapesCopy.push(clone);
-			console.log(clone === s)
-        }
-        // shapesCopy 配列には、shapes 配列の子要素が正確にコピーされる。
+    public shout() {
+        console.log(`[Cat]: mew!`)
     }
 }
 
-const app = new Application()
+export class Manager {
 
-app.businessLogic()
+    private showcase_: {[name: string]: Animal} = {};
+
+    public register(name: string, proto: Animal) {
+        this.showcase_[name] = proto;
+    }
+    public create(protoname: string) {
+        var proto: Animal = <Animal>this.showcase_[protoname];
+        return proto.createClone();
+    }
+}
+
+var manager: Manager = new Manager();
+var dog: Animal = new Dog();
+var cat: Animal = new Cat();
+
+manager.register("dog", dog);
+manager.register("cat", cat);
+
+var cpy1: Animal = manager.create("dog")
+var cpy2: Animal = manager.create("cat")
+
+cpy1.shout();
+cpy2.shout();
